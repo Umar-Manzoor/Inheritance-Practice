dfae51bcc8c062912d057601562b296b42e885a2
Umar-Manzoor <100792746+Umar-Manzoor@users.noreply.github.com>
2022-06-20 23:57:16 -0400

    Add files via upload

---
+++ Ghost.java
@@ -0,0 +1,31 @@
+public class Ghost extends TrickOrTreater{
+    private int transparency;
+    public Ghost(int transparency) {
+        super();
+        this.transparency = transparency;
+    }
+    public Ghost(String name, String neighborhood, int numCandy, int transparency) {
+        super(name, neighborhood, numCandy);
+        this.transparency = transparency;
+    }
+    public Ghost(Ghost other) {
+        other = this.
+    }
+    public void spook() {
+        if (transparency >= 10) {
+            System.out.println("Very spooky");
+        }
+        else if ( transparency >= 3) {
+            System.out.println("Boo!");
+        }
+        else if (transparency <= 2) {
+            System.out.println("Not very spooky");
+        }
+    }
+    public int getTransparency() {
+        return this.transparency;
+    }
+    public void setTransparency(int transparency) {
+        this.transparency = transparency;
+    }
+}
